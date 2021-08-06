48039e4ec7bfa119d635aaa7779eb5673db87152
Akwasi Asante-Krobea <Skybonne9@gmail.com>
2021-08-06 12:16:50 +0000

    added search bar

--- js/owl.js
+++ js/owl.js
@@ -1,1 +1,18 @@
 
+
+const searchBar = document.querySelector('.search');
+const clearAll = document.querySelector('.searchIcon');
+
+clearAll.addEventListener('click', (e)=>{
+    searchBar.value = "";
+    e.preventDefault();
+})
+
+function checkIfTyping(){
+    if(searchBar.value.length > 0){
+        clearAll.style.display = 'block';
+    }
+    else{
+        clearAll.style.display = 'none';
+    }
+}
